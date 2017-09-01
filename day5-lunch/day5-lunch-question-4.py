0051281d62372859cf7f15910516f096afd80044
Joshua Blundon <JoshuaMBlundon.gmail.com>
2017-09-01 16:18:38 -0400

    regressions compared individually

---
+++ day5-lunch/day5-lunch-question-4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import statsmodels.api as sm
+
+df = pd.read_csv(sys.argv[1], sep="\t")
+fpkm = df["FPKM"]
+
+for arg in sys.argv[2:]:
+    df_ad = pd.read_csv(arg, sep='\t', header=None, names = ["name", "size", "covered", "sum", "mean0", "mean"])
+    ad  = df_ad["mean"]
+    model = sm.OLS(ad,fpkm)
+    results = model.fit()
+    print results.summary()
+    
+# print results
+
