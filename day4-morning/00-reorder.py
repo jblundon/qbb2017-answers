7854c24830f68cc56692f1389d2cc4929b3f10fa
Joshua Blundon <JoshuaMBlundon.gmail.com>
2017-09-01 08:54:32 -0400

    day4 early morning intro to pandas

---
+++ day4-morning/00-reorder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+Usage: ./00-reorder.py <csv_file> <tsv_file>
+
+- Remove header
+- Reorder columns: sex, sample, stage
+- Subset "female" in sex column 
+- Convert delimter from comma to tab
+"""
+
+
+#f = open( sys.argv[1] )
+#
+#for i, line in enumerate( f ):
+#    if i == 0:
+#        continue
+#    fields = line.rstrip("\r\n").split(",")
+#    if fields[1] == "female":
+#        print "\t".join([ fields[1], fields [0], fields [2] ])
+#
+#f.close()
+#These lines of code do everything that the usage describes
+#But, now we can try to do this in pandas
+
+import sys
+import pandas as pd
+
+df = pd.read_csv( sys.argv[1] )
+#Specify desired column order
+coi = [ "sex", "sample", "stage"]
+#print df[coi]
+roi = df["sex"] == "female"
+#print roi
+df[coi][roi].to_csv( sys.argv[2], sep="\t", header=False, index=False )
